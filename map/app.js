fb2ea02dab249c0b8c5afa1ab3460564fc9d95d0
umersacirovic2006 <asacirovic0090@gmail.com>
2023-01-10 18:46:50 +0100

    idk

--- map/app.js
+++ map/app.js
@@ -1,9 +1,7 @@
 const arr = [1, 2, 3, 4, 5, 6]
 
-const newArr = arr.map(myFunction)
+const mapArr = arr.map(function(el){
+    return el.toString();
+})
 
-function myFunction(num) {
-    return num * 2
-}
-
-console.log(newArr);
+console.log(mapArr);
