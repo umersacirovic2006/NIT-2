9dd41611bd917d46850be8e9ad00248090d9c981
umersacirovic2006 <asacirovic0090@gmail.com>
2023-01-13 18:45:01 +0100

    <3

--- map/app.js
+++ map/app.js
@@ -1,7 +1,61 @@
-const arr = [1, 2, 3, 4, 5, 6]
+// MAP
 
-const mapArr = arr.map(function(el){
-    return el.toString();
-})
+// const arr = [2, 4, 6, 8, 10, 12, 14, 16]
 
-console.log(mapArr);
+// const mapArr = arr.map(function(el, i){
+
+//     if (i % 2 === 0){
+//         return (el + 2) * el;
+//     }
+//     else {
+//         return el
+//     }
+// })
+
+// console.log(mapArr);
+
+// FILTER
+
+// const numbers = [2, 3, 10, 11, 32, 23, 44, 57, 60];
+
+// const evenNumbers = numbers.filter(function(el, index) {
+//     if(el % 2 === 0) {
+//         return el;
+//     }
+// })
+
+// console.log("Prije", numbers);
+
+// console.log("Poslije", evenNumbers);
+
+const names = [
+    "Faris",
+    "Nedim",
+    "Esma",
+    "Aisa",
+    "Zein",
+    "Demir",
+    "Alen",
+    "Demir",
+    "Osman",
+    "Osman",
+    "Umer", // :D
+    "Asmir"
+]
+
+const shortNames = names.filter(function(name, i) {
+    if (name.length <= 4) {
+        return name;
+    }
+});
+
+console.log("Imena", names);
+
+console.log("Kratka imena", shortNames);
+
+const capitalA = names.filter(function(name) {
+    if (name.charAt(0).toUpperCase() === "A") return name;
+});
+
+console.log("P  rvo slovo A", capitalA);
+
