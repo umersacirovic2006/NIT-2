56dd7170b2ba1f60ef5421996b701ce688102aa1
umersacirovic2006 <asacirovic0090@gmail.com>
2023-02-02 17:56:20 +0100

    Objects

---
+++ examples/filter/app.js
@@ -0,0 +1,5 @@
+// const arr = ["Bilal", 115, [1000, "90"], true, null, 69, 12, "20", 10, "Math"]
+
+// const numbers = arr.filter((num) => {
+//     if(typeof num)
+// }) 
---
+++ objects/app.js
@@ -0,0 +1,18 @@
+const person = {
+    // "key-value pairs"
+    name: "Abdi",
+    lastName: "Al nasr",
+    age: 85,
+    wives: ["One", "Two", "Three", "Four"],
+    occupation: "Kupio NIS od Gazproma",
+    address: {
+        country: "Emirates",
+        city: "Novi Pazar",
+        street: "prvomajska",
+        number: 91,
+    }
+}
+
+person.height = 190;
+
+console.log(person);
